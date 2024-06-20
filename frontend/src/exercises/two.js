df75f1496735594345bcb21656d9ce19e55ee468
DevRamon83 <123456789+romanazzi.andrea@proton.me>
2024-06-20 07:38:08 +0200

    Complete exercise two

--- frontend/src/exercises/two.js
+++ frontend/src/exercises/two.js
@@ -1,3 +1,88 @@
-const two = [];
+const two = [
+  {
+    type: "input",
+    question: "Il pronome personale IO in inglese?",
+    answer: "I",
+    placeholder: "risposta",
+  },
+  {
+    type: "input",
+    question: "Il pronome personale TU in inglese?",
+    answer: "You",
+    placeholder: "risposta",
+  },
+  {
+    type: "input",
+    question: "Il pronome personale LUI in inglese?",
+    answer: "He",
+    placeholder: "risposta",
+  },
+  {
+    type: "input",
+    question: "Il pronome personale LEI in inglese?",
+    answer: "She",
+    placeholder: "risposta",
+  },
+  {
+    type: "input",
+    question: "Il pronome personale ESSO in inglese?",
+    answer: "It",
+    placeholder: "risposta",
+  },
+  {
+    type: "input",
+    question: "Il pronome personale NOI in inglese?",
+    answer: "We",
+    placeholder: "risposta",
+  },
+  {
+    type: "input",
+    question: "Il pronome personale VOI in inglese?",
+    answer: "You",
+    placeholder: "risposta",
+  },
+  {
+    type: "input",
+    question: "Il pronome personale LORO in inglese?",
+    answer: "They",
+    placeholder: "risposta",
+  },
+  {
+    type: "boolean",
+    question:
+      "I pronomi personali in inglese vanno scritti sempre con la maiuscola",
+    answer: true,
+  },
+  {
+    type: "input",
+    question: "La congiunzione E in inglese?",
+    answer: "and",
+    placeholder: "risposta",
+  },
+  {
+    type: "input",
+    question: "Traduci in inglese O (oppure)",
+    answer: "or",
+    placeholder: "risposta",
+  },
+  {
+    type: "input",
+    question: "Traduci in inglese MA",
+    answer: "but",
+    placeholder: "risposta",
+  },
+  {
+    type: "input",
+    question: "Così, dunque, perciò in inglese",
+    answer: "so",
+    placeholder: "risposta",
+  },
+  {
+    type: "input",
+    question: `Tuttora in inglese (anche inteso come "non ancora")`,
+    answer: "yet",
+    placeholder: "risposta",
+  },
+];
 
 export { two };
