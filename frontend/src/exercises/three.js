73028a98f97f364921f3e16fb49600bf949130f9
DevRamon83 <123456789+romanazzi.andrea@proton.me>
2024-07-01 20:15:25 +0200

    Add exercises for lesson number three

--- frontend/src/exercises/three.js
+++ frontend/src/exercises/three.js
@@ -1,3 +1,91 @@
-const three = [];
+const three = [
+  {
+    type: "input",
+    question: "Traduci in inglese: a me",
+    answer: "me",
+    placeholder: "risposta",
+  },
+  {
+    type: "input",
+    question: "Traduci in inglese: a te",
+    answer: "you",
+    placeholder: "risposta",
+  },
+  {
+    type: "input",
+    question: "Traduci in inglese: a lui",
+    answer: "him",
+    placeholder: "risposta",
+  },
+  {
+    type: "input",
+    question: "Traduci in inglese: a lei",
+    answer: "her",
+    placeholder: "risposta",
+  },
+  {
+    type: "input",
+    question:
+      "Qual è il pronome personale oggetto della terza persona neutrale?",
+    answer: "it",
+    placeholder: "risposta",
+  },
+  {
+    type: "input",
+    question: "Traduci in inglese: ci",
+    answer: "us",
+    placeholder: "risposta",
+  },
+  {
+    type: "input",
+    question: "Traduci in inglese: a voi",
+    answer: "you",
+    placeholder: "risposta",
+  },
+  {
+    type: "input",
+    question: "Traduci in inglese: a loro",
+    answer: "them",
+    placeholder: "risposta",
+  },
+  {
+    type: "multiple",
+    question:
+      "Come si traduce in inglese la formula: nè ... nè (ad esempio: nè buono nè cattivo)",
+    options: [
+      "or ... nor",
+      "as ... or",
+      "neither ... or",
+      "neither ... nor",
+      "or ... neither",
+    ],
+    answer: 3,
+  },
+  {
+    type: "input",
+    question: "Cosa significa in inglese: after?",
+    answer: "dopo",
+    placeholder: "risposta",
+  },
+  {
+    type: "multiple",
+    question:
+      "Come si possono tradurre in italiano le congiunzioni: although - tough",
+    options: ["nonostante", "purché", "al fine di", "allora", "sebbene"],
+    answer: 4,
+  },
+  {
+    type: "input",
+    question: "Cosa significa in inglese: as?",
+    answer: "come",
+    placeholder: "risposta",
+  },
+  {
+    type: "multiple",
+    question: "Come si può tradurre in italiano la formula: as if",
+    options: ["seppure", "sebbene", "quantunque", "anche se", "per quanto"],
+    answer: 3,
+  },
+];
 
 export { three };
