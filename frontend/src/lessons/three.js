1cf55c18dba0f2d34cb8cd03045eec3ff4c43893
DevRamon83 <123456789+romanazzi.andrea@proton.me>
2024-07-01 11:09:15 +0200

    Do lesson three

--- frontend/src/lessons/three.js
+++ frontend/src/lessons/three.js
@@ -1,5 +1,60 @@
-const three = {
-  ciao: "saluti",
-};
+const three = [
+  {
+    type: `title`,
+    text: `Pronomi personali oggetto`,
+  },
+  {
+    type: `paragraph`,
+    text: `Nella lezione precedente abbiamo studiato i pronomi personali soggetto, avrai notato sicuramente che invece di usare la terza persona singolare "egli" abbiamo usato le forme femminile e maschile (lei/lui); £questo perchè la terza persona singolare inglese può essere sia maschile che femminile, ma può essere anche neutra£ (it). Quando, ad esempio, parliamo di un libro questo soggetto non è nè maschile nè femminile (in inglese, perchè in italiano è un sostantivo maschile), per cui useremo la formula della terza persona neutra "it". Nella lezione di oggi studieremo i pronomi personali complemento, ma il loro funzionamento è identico, come puoi notare dal seguente schema riassuntivo:`,
+  },
+  {
+    type: `img`,
+    src: `/subjectPronTab.png`,
+  },
+  {
+    type: `paragraph`,
+    text: `Fai attenzione a memorizzare a quale persona (se maschile o femminile, singolare o plurale, prima o seconda o terza) si riferisce ogni pronome; nell'imparare a memoria queste parole, infatti, devi riuscire a costruire degli automatismi così da poterle usare al pari di come fai con i pronomi in italiano, in caso contrario quando parlerai dovrai fermarti continuamente per richiamare la parola corretta da usare. Non c'è ovviamente nulla di male nel fermarsi un attimo perchè non ci si ricorda come si pronuncia una parola, tuttavia £se non possiedi degli automatismi relativamente a pronomi, congiunzioni e preposizioni parlare diventerà un'attività estremamente snervante£ sia per te che per chi ti ascolta. `,
+  },
+  {
+    type: `recommended`,
+    title: [`Uso di ALTHOUGH, THOUGH e EVEN THOUGH`],
+    author: [`JoEnglish`],
+    link: [`https://www.youtube.com/watch?v=L85j5CG6KN8`],
+  },
+  {
+    type: `vocabulary`,
+    set: [
+      {
+        type: "pronomi pers. oggetto",
+        ita: [
+          "mi - a me",
+          "ti - a te",
+          "lo - a lui",
+          "la - a lei",
+          "3° pers. neutrale",
+          "ci - a noi",
+          "vi - a voi",
+          "li/le - a loro",
+        ],
+        eng: ["me", "you", "him", "her", "it", "us", "you", "them"],
+      },
+      {
+        type: "congiunzioni",
+        ita: [
+          "nè ... nè",
+          "dopo",
+          "anche se - sebbene",
+          "come - similmente",
+          "come se",
+        ],
+        eng: ["neither ... nor", "after", "although - though", "as", "as if"],
+      },
+    ],
+  },
+  {
+    type: `note`,
+    text: `Il nostro amico JoEnglish ci ha regalato un video in cui spiega per bene l'uso della congiunzione altough; ricordati sempre che Joe parla con accento britannico, per cui ricordati che se vuoi seguire il corso con la pronuncia americana non devi considerare l'accento che Joe usa nei suoi video.`,
+  },
+];
 
 export { three };
