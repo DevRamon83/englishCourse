189b3fe79fcadc67c832adc430cec1017b7eb43d
DevRamon83 <123456789+romanazzi.andrea@proton.me>
2024-07-01 11:42:11 +0200

    Replace paragraphContainerClass with imgContainerClass

--- frontend/src/component/creators/generateImgLesson.js
+++ frontend/src/component/creators/generateImgLesson.js
@@ -1,13 +1,13 @@
 import { leftContainerId } from "@/variables/idName";
 import defineClass from "../definers/defineClass";
 import appendElement from "../helpers/appendElement";
-import { paragraphContainerClass } from "@/variables/className";
+import { imgContainerClass } from "@/variables/className";
 
 export default function generateImgLesson(element) {
   const panel = document.getElementById(leftContainerId);
   const container = document.createElement("div");
 
-  defineClass(container, paragraphContainerClass);
+  defineClass(container, imgContainerClass);
   appendElement(panel, container);
   createImg(container, element.src);
 }
